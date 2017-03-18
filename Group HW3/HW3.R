21d6f5616a79cec458765f2525cd34845925e6cd
Billy Yuan <billy.yuan1@gmail.com>
2017-03-18 14:03:28 -0500

    updated function and RMD
    - added loop to test whether coefficients exceed M

--- Group HW3/HW3.R
+++ Group HW3/HW3.R
@@ -3,58 +3,60 @@ load(file = 'data.rdata')
 library(gurobi)
 library(glmnet)
 
-findBeta <- function(X_, y_, numZ) {
+findBeta <- function(X_, y_, k) {
   
   # Number of variables
-  k <- dim(X_)[2]
-  M <- 1000
+  numV <- dim(X_)[2]
+  M <- 1 # default value of M
   
   # Add z's
-  X_ <- cbind(X_, matrix(0,dim(X_)[1],k))
+  X_ <- cbind(X_, matrix(0,dim(X_)[1],numV))
   
   # Make constraints
   
-  # Our x vector has 2*k columns: the first k columns represent the betas for each variable. The last k are z variables
+  # Our x vector has 2*numV columns: the first numV columns represent the betas for each variable. The last numV are z variables
   
   # Constraint 1: Sum of z is less than or equal to k
-  A <- matrix(0,1,2*k) # make A matrix.
-  A[,(k+1):(2*k)] <- 1 # last k columns are for z variables
+  A <- matrix(0,1,2*numV) # make A matrix.
+  A[,(numV+1):(2*numV)] <- 1 # last numV columns are for z variables
   
-  b <- c(numZ)
+  b <- c(k)
   
   
   # Constraint 2A: -beta - Mz <= 0 
   
-  A.2.1 <- matrix(0,k,2*k)
-  A.2.1[1:k,1:k] <- diag(-1,k)
-  A.2.1[1:k, (k+1):(2*k)] <- diag(-M, k)
+  A.2.1 <- matrix(0,numV,2*numV)
+  A.2.1[1:numV,1:numV] <- diag(-1,numV)
+  A.2.1[1:numV, (numV+1):(2*numV)] <- diag(-M, numV)
   
-  b <- append(b, rep(0,k))
+  b <- append(b, rep(0,numV))
   
   A <- rbind(A, A.2.1) # append rows of A matrix
   
   # Constraint 2B: beta - Mz <= 0 
-  A.2.2 <- matrix(0,k, 2*k)
-  A.2.2[1:k, 1:k] <- diag(1,k)
-  A.2.2[(1:k), (k+1):(2*k)] <- diag(1, k)
-  A.2.2[1:k, (k+1):(2*k)] <- diag(-M, k)
+  A.2.2 <- matrix(0,numV, 2*numV)
+  A.2.2[1:numV, 1:numV] <- diag(1,numV)
+  A.2.2[(1:numV), (numV+1):(2*numV)] <- diag(1, numV)
+  A.2.2[1:numV, (numV+1):(2*numV)] <- diag(-M, numV)
   
-  b <- append(b, rep(0,k))
+  b <- append(b, rep(0,numV))
   
   A <- rbind(A, A.2.2)
   
   # Make dir
-  dir <- rep("<=", (2*k)+1)
+  dir <- rep("<=", (2*numV)+1)
 
   
   # Create min function
   Q <- .5 * t(X_) %*% (X_)
-  c <- -.5 * t(X_) %*% y_
-  alpha <- t(y_) %*% y_
+  c <- -1*t(t(y_) %*% X_)
+  alpha <- .5 * t(y_) %*% y_
   
+  # Define model
   model <- list()
   params <- list(Method=2)
   
+  # Set model parameters
   model$A <- A
   model$Q <- Q
   model$obj <- c
@@ -62,11 +64,37 @@ findBeta <- function(X_, y_, numZ) {
   model$modelsense <- "min"
   model$rhs <- b
   model$sense <- dir
-  model$vtype <- append(rep('C',k),rep('B',k))
+  model$vtype <- append(rep('C',numV),rep('B',numV))
   
-  gurobi(model, params)
-  # cbind(A, dir, b)
+  # Solve
+  model.solve <- gurobi(model, params)
+  model.coef <- model.solve$x[1:numV]
+  
+  # Check if the magnitude of any coefficient in the solution is greater than M. If such a coefficient exists, double M and resolve.
+  # Repeat this until there are no coefficients whose magnitude exceeds M
+  
+  while (sum(abs(model.coef) >= M) != 0) {
+    M <- M * 2
+    model.solve <- gurobi(model, params)
+    model.coef <- model.solve$x[1:numV]
+  }
+  
+  model.solve
+  
 }
 
-# Check if our solution matches actual solution
-findBeta(X[1:dim(X)[1],1:64], y, 8)$x[65:128] == beta_real
+solution <- findBeta(X, y, 8)$x[1:64]
+(solution != 0) == (beta_real != 0) # Check betas match
+
+# Use glmnet to find 8 betas
+
+lasso <- glmnet(X, y, alpha=1, family="gaussian", intercept=FALSE)
+
+coef(lasso)[,5]
+sum(coef(lasso)[,5] != 0)
+
+# Make predictions
+predict <- X %*% solution
+actual <- X %*% beta_real
+sum((predict-actual)**2) / sum(actual**2)
+
