1199d61f3e8809b32707998eb1e7c3b72acf217b
Billy Yuan <billy.yuan1@gmail.com>
2017-03-27 15:10:15 -0500

    added miqp error

--- Group HW3/project3_g4.R
+++ Group HW3/project3_g4.R
@@ -84,16 +84,19 @@ findBeta <- function(X_, y_, k) {
   
 }
 
+# Store betas from MIQP
 solution <- findBeta(X, y, 8)$x[1:64]
-(solution != 0) == (beta_real != 0) # Check betas match
+# Check betas match. Sum should be 64
+sum((solution != 0) == (beta_real != 0))
 
-#Apply Lasso to the given data
+# Apply Lasso to the given data 
 lasso <- glmnet(X, y, alpha=1,intercept=FALSE, family='gaussian')
-plot(lasso, xvar = "lambda", label = TRUE)
 
-mat.coef <- matrix(0,length(lasso$lambda),3)
-actual <- X %*% beta_real
+# Build matrix that stores number of coefficients per lambda. The next loop will write the number of coefficients per lambda
+mat.coef <- matrix(0,length(lasso$lambda),3) # store number of coefficients
+actual <- X %*% beta_real # actual values
 
+# Generate number of coefficients and prediction error for each lambda
 for (model in 1:length(lasso$lambda)){
   num.coef <- coef(lasso, s = lasso$lambda[model])
   predict_Lasso <- X %*% num.coef[2:65]
@@ -101,8 +104,14 @@ for (model in 1:length(lasso$lambda)){
   mat.coef[model,] <- c(lasso$lambda[model],sum(num.coef != 0),error)
 }
 
+# Store prediction error from MIQP
+error_MIQP <- sum(((X %*% solution) - actual)**2) / sum(actual**2)
 
+### Questions 2 and 3 Plots
+
+# Q2
 plot(mat.coef[,1], mat.coef[,2],xlab="lambda", ylab="coefficients", main="Lasso: Coefficients vs. Lamda")
-plot(mat.coef[,1], mat.coef[,3], xlab="lambda", ylab="errors", main="Lasso: Lambda vs. error")
+# Q3
+plot(mat.coef[,1], mat.coef[,3], xlab="lambda", ylab="errors", main="Lasso: Lambda vs. error") 
 abline(h = error_MIQP,col = "red")
 legend("topleft","MIQP error",lty=1,cex=1,bty="n",col="red")
